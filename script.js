6182387c3d1afa46d46259527d66b3c9459e1ff0
harshadahp <149163459+harshadahp@users.noreply.github.com>
2023-10-27 04:39:22 -0700

    java script file

---
+++ script.js
@@ -0,0 +1,24 @@
+/*var first=0;
+function autoslide(){
+  setTimeout(autoslide,4000);
+  var pics;
+  const img=document.querySelectorAll('img');
+for(pics=0;pics<img.length;pics++)
+    {
+        img[pics].style.display="none";
+    }
+    first++;
+    if(first>img.length){
+      first =1;
+    }
+    img[first -1].style.display="block";
+}
+autoslide();
+*/
+
+$('#menu-btn').click(function(){
+  $('.nav ul').addClass('active')
+});
+$('#menu-close').click(function(){ 
+  $('.nav ul').removeClass('active')
+});
